2ba4d8c83632d6e0a1bb8cc3dccaf565428f82e6
KutsiGiira <haytamboualkhours@gmail.com>
2025-03-08 15:08:27 +0000

    making crud rest api

--- LearningDirectory/routing/index.js
+++ LearningDirectory/routing/index.js
@@ -3,28 +3,46 @@ const app = express();
 const port = 8080;
 const fs = require("fs");
 
+app.use(express.json()); 
+
 const path = JSON.parse(fs.readFileSync("./data/tours-simple.json"));
-app.get("/tours", (req, res) => {
-    res.status(200).json(path);
-})
+
+const getAllTours = (req, res) =>{
+    res.send(path)
+}
+
+const getTour = (req, res) => {
+    const id = req.params.id * 1;
+    const tour = path.find(e => e.id === id);
+    res.json(tour);
+}
+
+const UpdateTour = (req, res) => {
+    res.send("Update")
+    
+}
+
+const deleteTour = (req, res) => {
+    res.send("Deleted")
+    
+}
+
+const postTour = (req, res) => {
+    const newTour = {
+        id: path.length + 1,
+        name: req.body.name,
+    }
+    path.push(newTour);
+
+    fs.writeFile("./data/tours-simple.json", JSON.stringify(path), err => {
+        res.status(201).json(path)
+    })
+    res.send("Added");
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.route("/tours").get(getAllTours).post(postTour);
+app.route("/tours/:id").get(getTour).patch(UpdateTour).delete(deleteTour);
 
 app.listen(port, () => {
     console.log("App running in port " + port)
